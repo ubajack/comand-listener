20166fd9af105bf5bbabb8f9d260f3f88659e59b
ubajack <jacques.bach@ubalia.fr>
2023-11-24 15:53:04 +0100

    Modify client

--- client/app.js
+++ client/app.js
@@ -1,17 +1,38 @@
-const axios = require('axios');
-require('dotenv').config();
-
-const baseURL = process.env.BASE_URL;
+const baseURL = 'http://localhost:3000';
 
 const instance = axios.create({
-    baseURL
+    baseURL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
-instance.get('/')
-    .then(function (response) {
-        const { data } = response;
-        console.log('Got data:', data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+function handleResponse(response) {
+    const { data } = response;
+    console.log('Got data:', data);
+}
+
+function handleError(error) {
+    console.error(error);
+}
+
+function testConnection() {
+    instance.get('/')
+        .then(handleResponse)
+        .catch(handleError);
+}
+
+function sendSampleData() {
+    const sampleData = {
+        label: '/hello',
+        answer: 'Bonjour $user !'
+    };
+
+    instance.post('/sample', sampleData)
+        .then(handleResponse)
+        .catch(handleError);
+}
+
+function sendRequest() {
+    sendSampleData();
+}
