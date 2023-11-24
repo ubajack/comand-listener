bf18bdbcb73e8440b99521ed0cf3d3f69e69362a
ubajack <jacques.bach@ubalia.fr>
2023-11-24 15:53:47 +0100

    Add sample route

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.post('/sample', (req, res) => {
+    console.log(req.headers, req.body);
+    res.status(200).send({ data: 'ok' });
 });
 
 app.listen(port, () => {
