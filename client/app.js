1ce1bb5f5dfdc301e9c7745aedadace6edd19950
ubajack <jacques.bach@ubalia.fr>
2023-11-24 17:40:03 +0100

    Add some comments

--- client/app.js
+++ client/app.js
@@ -8,21 +8,25 @@ const instance = axios.create({
     }
 });
 
+// Callback pour les réponses de l'API
 function handleResponse(response) {
     const { data } = response;
     console.log('Got data:', data);
 }
 
+// Callback pour les erreurs de l'API
 function handleError(error) {
     console.error(error);
 }
 
+// Envoi d'une requête pour tester la configuration
 function testConnection() {
     instance.get('/')
         .then(handleResponse)
         .catch(handleError);
 }
 
+// Envoi de données d'exemple
 function sendSampleData() {
     const sampleData = {
         label: '/hello',
@@ -34,6 +38,7 @@ function sendSampleData() {
         .catch(handleError);
 }
 
+// Callback appelé suite au clic du bouton
 function sendRequest() {
     sendSampleData();
 }
